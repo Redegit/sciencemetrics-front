db7cf717453eb923b8671111dabe8c491f7df1bf
Redegit <dima070603@gmail.com>
2025-04-27 04:45:49 +0300

    feat(graph organizations): add onClick table with publications

--- src/pages/dashboards/Orgs.tsx
+++ src/pages/dashboards/Orgs.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { FilterItem, FiltersForm } from "../../types";
+import {
+  FilterItem,
+  FiltersForm,
+  GraphTableData,
+  GraphTables,
+} from "../../types";
 import { useForm } from "react-hook-form";
 import { Graph } from "../../components/Graph";
+import { request } from "../../api/request";
 
 type OrganizationFilters = {
   keywords: "select";
@@ -36,9 +42,42 @@ export const ORGS = React.memo(() => {
     },
   ];
 
+  const graphTables: GraphTables = {
+    node: {
+      title: "Публикации организации",
+      columns: [
+        {
+          label: "Название",
+          name: "title",
+        },
+        { label: "Журнал", name: "journal" },
+        { label: "Год", name: "year" },
+      ],
+      getData: async ({ nodeId, page = 1 }) => {
+        return (await request.get(
+          `/graph/organizations/table/node?id=${nodeId}&page=${page}`
+        )) as GraphTableData;
+      },
+    },
+    link: {
+      title: "Совместные публикации",
+      columns: [
+        { label: "Название", name: "title" },
+        { label: "Журнал", name: "journal" },
+        { label: "Год", name: "year" },
+      ],
+      getData: async ({ source, target }) => {
+        return (await request.get(
+          `/graph/organizations/table/link?source=${source}&target=${target}`
+        )) as GraphTableData;
+      },
+    },
+  };
+
   return (
     <Graph
       graphName="organizations"
+      graphTables={graphTables}
       {...{ control, handleSubmit, reset, filters }}
     />
   );
