2b653b85c8b4efa17f2a50b1c1a0f44f35e2fbdd
Redegit <dima070603@gmail.com>
2025-05-14 02:46:46 +0300

    fix

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -105,7 +105,7 @@ export const LoginPage = () => {
               )}
 
               <div className="btnEnter">
-                <button className="button">Вход ахаха</button>
+                <button className="button">Вход</button>
               </div>
             </form>
           </div>
